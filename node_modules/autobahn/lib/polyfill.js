c2a39c09b46a9296b3f7728288b0931bdf9c0985
Giuseppe Raveduto <giuseppe.raveduto@eng.it>
2017-11-16 11:57:36 +0100

    removed node_modules folder

--- node_modules/autobahn/lib/polyfill.js
+++
@@ -1,7 +0,0 @@
-require('./polyfill/object');
-require('./polyfill/array');
-require('./polyfill/string');
-require('./polyfill/function');
-require('./polyfill/console');
-require('./polyfill/typedarray');
-require('./polyfill/json');
--- node_modules/autobahn/lib/polyfill/string.js
+++
@@ -1,7 +0,0 @@
-// Add ECMA262-5 string trim if not supported natively
-//
-if (!('trim' in String.prototype)) {
-    String.prototype.trim= function() {
-        return this.replace(/^\s+/, '').replace(/\s+$/, '');
-    };
-}
