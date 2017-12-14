0d342ffa71d9d090b5a6116ee05d246ee61440c8
Sayed Tauseef Naqvi <tauseefnaqvi@Sayeds-MacBook-Air.local>
2017-12-15 03:25:07 +0530

    create processRequestCtrl for tx/hash/block search function

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -21,58 +21,6 @@ angular.module('ethExplorer')
           }
         });
 
-        $scope.processRequest= function(){
-            var requestStr = $scope.ethRequest;
-
-
-            if (requestStr!==undefined){
-
-                // maybe we can create a service to do the reg ex test, so we can use it in every controller ?
-
-                var regexpTx = /[0-9a-zA-Z]{64}?/;
-                //var regexpAddr =  /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/; // TODO ADDR REGEX or use isAddress(hexString) API ?
-                var regexpAddr = /^(0x)?[0-9a-f]{40}$/; //New ETH Regular Expression for Addresses
-                var regexpBlock = /[0-9]{1,7}?/;
-
-                var result =regexpTx.test(requestStr);
-                if (result===true){
-                    goToTxInfos(requestStr)
-                }
-                else{
-                    result = regexpAddr.test(requestStr.toLowerCase());
-                    if (result===true){
-                        goToAddrInfos(requestStr.toLowerCase())
-                    }
-                    else{
-                        result = regexpBlock.test(requestStr);
-                        if (result===true){
-                            goToBlockInfos(requestStr)
-                        }
-                        else{
-                            console.log("nope");
-                            return null;
-                        }
-                    }
-                }
-            }
-            else{
-                return null;
-            }
-        };
-
-
-        function goToBlockInfos(requestStr){
-            $location.path('/block/'+requestStr);
-        }
-
-        function goToAddrInfos(requestStr){
-            $location.path('/address/'+requestStr.toLowerCase());
-        }
-
-        function goToTxInfos (requestStr){
-             $location.path('/tx/'+requestStr);
-        }
-
         function updateStats() {
             web3.eth.getBlockNumber(function (err,currentBlockNumber) {
                 if(err)
